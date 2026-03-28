d25b98ea847c0e3ef0a7a0331fe8f46d51055367
MdKaif36 <mdkaif8036@gmail.com>
2026-03-28 12:04:41 +0530

    Commit Branch 2

--- src/main/java/GitExample.java
+++ src/main/java/GitExample.java
@@ -1,8 +1,6 @@
 public class GitExample {
     public static void main(String[] args) {
         System.out.println("Branch 1 is open");
-        int b=6;
-        System.out.println(b);
 
     }
 }
