9ec2483d893c0f012c7b8f37580b2f169962673b
MdKaif36 <mdkaif8036@gmail.com>
2026-03-28 13:25:03 +0530

    Commit branch FEATURE

--- src/main/java/GitExample.java
+++ src/main/java/GitExample.java
@@ -1,6 +1,11 @@
 public class GitExample {
     public static void main(String[] args) {
-        System.out.println("Main branch is open");
-
+        System.out.println("Hello World");
+        login();
     }
+    public static void login() {
+        System.out.println("Login Feature Added");
+    }
+
 }
+
